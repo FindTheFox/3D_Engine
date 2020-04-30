8c61b54eef9984fc3f8648dbe26d8c8ad5e2b1ce
FindTheFox <samuel.neveugall@yahoo.fr>
2020-04-30 04:47:55 +0200

    New color

---
+++ allobj.sh
@@ -0,0 +1,2 @@
+
+./DooM ressources/cube.obj ressources/lathe2.obj ressources/mountain ressources/three-big.obj ressources/mp5k.obj ressources/torus.obj ressources/demon.obj ressources/lemur.obj ressources/katana.obj ressources/monkey2.obj ressources/teapot
